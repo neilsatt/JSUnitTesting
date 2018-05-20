feefb227f983ef5def3f6294677645a9708fb728
neilsatt <neilsatt@gmail.com>
2018-05-19 19:52:53 -0600

    updates

--- textUtilities.js
+++ textUtilities.js
@@ -4,10 +4,18 @@ var expect = require('chai').expect;
 expect(true).to.be.true;
 
 function titleCase (title) {
-    return title;
+    var words = title.split(' '); // individual words in title
+    var titleCasedWords = words.map(function (word) {
+        return word[0].toUpperCase() + word.substring(1);
+    });
+    return titleCasedWords.join(' '); // put them back together
+     
 }
 
 expect(titleCase('paris, texas')).to.be.a('string');
+expect(titleCase('a')).to.equal('A');
+expect(titleCase('vertigo')).to.equal('Vertigo');
+
 expect(titleCase('paris, texas')).to.equal('Paris, Texas');
 
 
