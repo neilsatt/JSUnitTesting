c67020c5a46608b1da3ba179af95d2451925609a
neilsatt <neilsatt@gmail.com>
2018-05-22 19:04:24 -0600

    add project

---
+++ game_logic/ship_methods.js
@@ -0,0 +1,6 @@
+function checkForShip () {
+
+}
+
+module.exports.checkForShip = checkForShip;
+
---
+++ test/main_test.js
@@ -0,0 +1,9 @@
+var expect = require('chai').expect;
+
+// Test Suite
+describe('Mocha', function() {
+    // Test Spec (unit test)
+    it('should run our tests using npm', function() {
+        expect(true).to.be.ok; // ok checks if value is truthy
+    });
+});
